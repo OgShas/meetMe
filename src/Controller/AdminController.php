88b07b716f2cb86c0224d5f27372d9ccd407d789
OgShas <shasog@gmail.com>
2025-11-08 09:25:50 +0200

    move to symfony 6.4

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -5,15 +5,17 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+
 
 class AdminController extends AbstractController
 {
     #[Route('/admin', name: 'app_admin')]
+    #[IsGranted('ROLE_ADMIN')]
     public function index(): Response
     {
-        $this->denyAccessUnlessGranted('ROLE_ADMIN');
         return $this->render('admin/index.html.twig', [
-            'controller_name' => 'AdminController',
+            'controller_name' => 'AdminController'
         ]);
     }
 }
